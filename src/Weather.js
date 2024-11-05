72693a29bf3120ea11d7655e7435ae0e1a2f28f9
RAFiasyam <rafi.asyam23@gmail.com>
2024-11-05 18:45:01 +0700

    this before styling

---
+++ src/Weather.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Weather = () => {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=9c503b028748d1e120c892a094dee3d8`
+      );
+      setWeatherData(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleInputChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchData();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter city name"
+          value={city}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Get Weather</button>
+      </form>
+      {weatherData ? (
+        <>
+          <h2>{weatherData.name}</h2>
+          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>Description: {weatherData.weather[0].description}</p>
+        </>
+      ) : (
+        <p>Loading weather data...</p>
+      )}
+    </div>
+  );
+};
+
+export default Weather; 
